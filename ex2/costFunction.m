6b6961e998f8df3b112594ae6c6590c78d971e97
Vitalii Lakusta <lakusta96@gmail.com>
2016-09-11 19:27:49 +0300

    Implement cost function and gradient descent for logistic regression

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -4,29 +4,21 @@ function [J, grad] = costFunction(theta, X, y)
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
 
-% Initialize some useful values
-m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
+numberOfTrainingExamples = length(y);
+costSummation = 0;
+for i=1:numberOfTrainingExamples
+    costSummation += -y(i)*log(sigmoid(theta' * X(i, :)')) ...
+        - (1 - y(i))*log(1 - sigmoid(theta' * X(i, :)'));
+end;
+J = (1 / numberOfTrainingExamples) * costSummation;
+
 grad = zeros(size(theta));
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
+for j=1:size(theta)
+    for i=1:numberOfTrainingExamples
+        grad(j) += (sigmoid(theta' * X(i, :)') - y(i)) * X(i, j);
+    end;
+end;
+grad /= numberOfTrainingExamples;
 
 end
