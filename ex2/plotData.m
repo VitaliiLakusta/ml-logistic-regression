dd8deb559b21f927d3095a3a87fd9cb8dbbf365f
Vitalii Lakusta <lakusta96@gmail.com>
2016-09-11 17:57:05 +0300

    replace double quotes with single quotes; follow convention

--- ex2/plotData.m
+++ ex2/plotData.m
@@ -15,10 +15,10 @@ figure; hold on;
 positiveClassIndices = find(y == 1);
 negativeClassIndices = find(y == 0);
 
-plot(X(positiveClassIndices, 1), X(positiveClassIndices, 2), "g+", "LineWidth", 2, ...
-    "MarkerSize", 7);
-plot(X(negativeClassIndices, 1), X(negativeClassIndices, 2), "ro", "LineWidth", 2, ...
-    "MarkerSize", 7);
+plot(X(positiveClassIndices, 1), X(positiveClassIndices, 2), 'g+', 'LineWidth', 2, ...
+    'MarkerSize', 7);
+plot(X(negativeClassIndices, 1), X(negativeClassIndices, 2), 'ro', 'LineWidth', 2, ...
+    'MarkerSize', 7);
 
 hold off;
 
