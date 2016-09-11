8d1ccc3096c373e81cf85083ce85a55b9f9321af
Vitalii Lakusta <lakusta96@gmail.com>
2016-09-11 17:55:19 +0300

    plot data set of admitted and not admitted students from a training set

--- ex2/plotData.m
+++ ex2/plotData.m
@@ -1,5 +1,5 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
+%PLOTDATA Plots the data points X and y into a new figure
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
@@ -12,17 +12,13 @@ figure; hold on;
 %               examples and 'ko' for the negative examples.
 %
 
+positiveClassIndices = find(y == 1);
+negativeClassIndices = find(y == 0);
 
-
-
-
-
-
-
-
-% =========================================================================
-
-
+plot(X(positiveClassIndices, 1), X(positiveClassIndices, 2), "g+", "LineWidth", 2, ...
+    "MarkerSize", 7);
+plot(X(negativeClassIndices, 1), X(negativeClassIndices, 2), "ro", "LineWidth", 2, ...
+    "MarkerSize", 7);
 
 hold off;
 
